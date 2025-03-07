c6d791a181bb33ca885c711fcd958bdf0092fa89
gl-aarav <gl.aarav@gmail.com>
2025-03-06 22:08:04 -0800

    Updates

--- AccessibilityRunner.java
+++ AccessibilityRunner.java
@@ -1,6 +1,14 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Font;
+
+import java.awt.event.KeyListener;
+import java.awt.event.MouseListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class AccessibilityRunner 
 {
